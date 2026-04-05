2a72ae050e6c87f0fb60fea7d7201b870a0407cf
Adam-Dulc <Adam-Dulc@hotmail.com>
2026-04-05 19:52:56 +0100

    Added cinematic video to film archive
    Added cinematic video to film archive, began to change Chloe fisher page, Added new image to tv and press packs page

--- script.js
+++ script.js
@@ -234,3 +234,19 @@ window.addEventListener("DOMContentLoaded", () => {
   const firstStrike = 1000 + Math.random() * 1000;
   setTimeout(triggerLightning, firstStrike);
 });
+
+/* CLOCK */
+
+function updateClock() {
+  const now = new Date();
+
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+
+  document.getElementById("clock").textContent =
+    `${hours}:${minutes}:${seconds}`;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
